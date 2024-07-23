3bb8eb0b15c5b7e7db67e0cd49843a3818fc655a
Karen Gonzalez <karenfggutierrez@gmail.com>
2024-07-23 08:55:05 -0600

    method change

--- Task4/users-authentication-backend/api/login.js
+++ Task4/users-authentication-backend/api/login.js
@@ -1,6 +1,18 @@
+const express = require('express');
+const app = express();
 const mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const cors = require('cors');
+
+app.use(express.json());
+
+const corsOptions = {
+  origin: 'https://users-authentication-psi.vercel.app',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -14,19 +26,17 @@ db.connect(err => {
   console.log('MySQL Connected...');
 });
 
-module.exports = (req, res) => {
-  if (req.method === 'POST') {
-    const { email, password } = req.body;
-    db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
-      if (err) return res.status(500).json({ message: 'Error on the server.' });
-      if (results.length === 0) return res.status(404).json({ message: 'No user found.' });
-      const user = results[0];
-      const passwordIsValid = bcrypt.compareSync(password, user.password);
-      if (!passwordIsValid) return res.status(401).json({ auth: false, token: null });
-      const token = jwt.sign({ id: user.id }, 'secret', { expiresIn: 86400 });
-      res.status(200).json({ auth: true, token: token });
-    });
-  } else {
-    res.status(405).send({ message: 'Method Not Allowed' });
-  }
-};
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
+    if (err) return res.status(500).json({ message: 'Error on the server.' });
+    if (results.length === 0) return res.status(404).json({ message: 'No user found.' });
+    const user = results[0];
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) return res.status(401).json({ auth: false, token: null });
+    const token = jwt.sign({ id: user.id }, 'secret', { expiresIn: 86400 });
+    res.status(200).json({ auth: true, token: token });
+  });
+});
+
+module.exports = app;
