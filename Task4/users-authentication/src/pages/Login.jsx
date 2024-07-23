5d59caca36a1dcd9ea1d05a9b9eba2dd2dbf1065
Karen Gonzalez <karenfggutierrez@gmail.com>
2024-07-23 09:17:39 -0600

    console.error to console.log

--- Task4/users-authentication/src/pages/Login.jsx
+++ Task4/users-authentication/src/pages/Login.jsx
@@ -35,7 +35,7 @@ export const LoginPage = () => {
       }
     } catch (error) {
       setErrorAuth(true);
-      console.error(error);
+      console.log(error + "This is the error");
     }
   };
 
