47c8b1183d5663efece75e523a0e15d62965f338
Karen Gonzalez <karenfggutierrez@gmail.com>
2024-07-23 20:10:34 -0600

    changed sql credentials

--- Task4/users-authentication-backend/server.js
+++ Task4/users-authentication-backend/server.js
@@ -25,10 +25,10 @@ const verifyToken = (req, res, next) => {
 };
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'itransition123.',
-    database: 'users_authentication'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
